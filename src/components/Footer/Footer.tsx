83e805d69d1f1f62374ce392528fd35add615ec8
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-04-24 17:29:30 +0530

    New

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
             </div>
           </Link>
           <p>
-            We are dedicated to delivering customized and high quality web
+            dedicated to delivering customized and high quality web
             solutions that meet your specific business needs. Our team builds
             responsive, search engine optimized and user-friendly websites that
             attract visitors and generate business opportunities.
